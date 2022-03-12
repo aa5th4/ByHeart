b5e8f1bb1890df70422fce17ebf6e19660c816a8
aa5th4 <2019ume0217@iitjammu.ac.in>
2022-03-12 16:27:48 +0530

    connected to polygon test network

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -5,9 +5,15 @@ require('@nomiclabs/hardhat-waffle');
 module.exports = {
   solidity: '0.8.0',
   networks: {
-    ropsten: {
-      url: 'https://eth-ropsten.alchemyapi.io/v2/lCVxvU5Edq00JBzNG8E77sJI1TF-bTy8 ',
-      accounts: ['2f99db8cdb04655028eee1dc98230925202f6b3e010e43fad2883b4bea90a1a3'],
+    hardhat: {
+      chainId: 1337
     },
+
+    Mumbai: {
+
+      url: `https://rpc-mumbai.maticvigil.com/`,
+      accounts: [`a71fdf3730b4f279b83c65175536d92d563587f72179d74552f8e1f9dc3444c9`]
+    },
+
   },
 };
