7fb9902a145e142609d5b87e0c36ac0283a555bd
Surabhi Gupta <surabhigupta1519@gmail.com>
2022-03-13 06:06:15 +0530

    made changes

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { HiMenuAlt4 } from "react-icons/hi";
 import { AiOutlineClose } from "react-icons/ai";
 
-import ByHeart from "../../images/ByHeart.png";
+import Logo from "../../images/logo.png";
 
 const NavBarItem = ({ classprops }) => (
   <>
@@ -19,7 +19,7 @@ const Navbar = () => {
   return (
     <nav className="w-full flex md:justify-center justify-between items-center p-4">
       <div className="md:flex-[0.5] flex-initial justify-center items-center">
-        <img src={ByHeart} alt="logo" className="w-32 cursor-pointer " />
+        <img src={Logo} alt="logo" className="w-40 cursor-pointer " />
       </div>
       <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
         <NavBarItem />
